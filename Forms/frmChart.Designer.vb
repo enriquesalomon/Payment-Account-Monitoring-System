2ca19e06f54b8cfc9bc122258c5716b1560dd9a6
enriquesalomon <king92salomon@gmail.com>
2021-07-21 14:51:21 -0700

    Updated

---
+++ Forms/frmChart.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmChart
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Chart2 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.Chart2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Chart1
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(58, 61)
+        Me.Chart1.Name = "Chart1"
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Me.Chart1.Series.Add(Series1)
+        Me.Chart1.Size = New System.Drawing.Size(353, 479)
+        Me.Chart1.TabIndex = 0
+        Me.Chart1.Text = "Chart1"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Gray
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(887, 37)
+        Me.Panel1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button1.Location = New System.Drawing.Point(812, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 37)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Close"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(55, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Weekly"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(488, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Monthly"
+        '
+        'Chart2
+        '
+        ChartArea2.Name = "ChartArea1"
+        Me.Chart2.ChartAreas.Add(ChartArea2)
+        Legend2.Name = "Legend1"
+        Me.Chart2.Legends.Add(Legend2)
+        Me.Chart2.Location = New System.Drawing.Point(491, 61)
+        Me.Chart2.Name = "Chart2"
+        Series2.ChartArea = "ChartArea1"
+        Series2.Legend = "Legend1"
+        Series2.Name = "Series1"
+        Me.Chart2.Series.Add(Series2)
+        Me.Chart2.Size = New System.Drawing.Size(353, 166)
+        Me.Chart2.TabIndex = 3
+        Me.Chart2.Text = "Chart2"
+        '
+        'frmChart
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(887, 622)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Chart2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Chart1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmChart"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "frmChart"
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.Chart2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Chart2 As DataVisualization.Charting.Chart
+End Class
---
+++ Forms/frmChart.vb
@@ -0,0 +1,89 @@
+﻿
+
+Imports System.Data.OleDb
+Imports System.Windows.Forms.DataVisualization.Charting
+
+Public Class frmChart
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmChart_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        generateGraphSale()
+    End Sub
+    Sub generateGraphSale()
+
+
+        'Call connect(condbPOS)
+        'mycommand = mysqlconn.CreateCommand
+        'mycommand.CommandText = "Select Top 1  * from StudentAccount order by ID DESC"
+        'myadapter.SelectCommand = mycommand
+        'myadapter.Fill(mydataset, "StudentAccount")
+        'mydataTable = mydataset.Tables("StudentAccount")
+        'mysqlreader = mycommand.ExecuteReader()
+        'While mysqlreader.Read
+        '    Me.Chart1.Series("Series1").Points.AddXY(("Profit of the Day").ToString, mysqlreader("TotalNetAmount"))
+        '    Me.Chart1.Series("Series1").Points.AddXY(("Discounted of the Day").ToString, mysqlreader("TotalDiscAmount"))
+        'End While
+        'mysqlreader.Close()
+        'mysqlconn.Close()
+
+        'Call connect(condbPOS)
+        'mycommand = mysqlconn.CreateCommand
+        ''mycommand.CommandText = "Select DatePart(yyyy,[ExpenseDate]) As Year,Category from SchoolExpenses Group By Category,DatePart(yyyy,[ExpenseDate])"
+        'mycommand.CommandText = "SELECT Year(ExpenseDate) as years,Category,Amount FROM SchoolExpenses Group By Category,Year(ExpenseDate),Amount"
+
+
+        'myadapter.SelectCommand = mycommand
+        'myadapter.Fill(mydataset, "SchoolExpenses")
+        'mydataTable = mydataset.Tables("SchoolExpenses")
+        'mysqlreader = mycommand.ExecuteReader()
+        'While mysqlreader.Read
+        '    MsgBox(mysqlreader("years").ToString + " " + mysqlreader("category").ToString)
+        '    Me.Chart1.Series("Series1").Points.AddXY(("Sales").ToString, mysqlreader("Amount"))
+        '    Me.Chart1.Series("Series1").Points.AddXY(("Discounted of the Day").ToString, mysqlreader("years"))
+        'End While
+        'mysqlreader.Close()
+        'mysqlconn.Close()
+
+        Dim da As New OleDbDataAdapter("SELECT Year(ExpenseDate) as years,Category,Amount FROM SchoolExpenses Group By Category,Year(ExpenseDate),Amount", condbPOS)
+        Dim ds As New DataSet
+
+
+        da.Fill(ds, "Category")
+        Chart1.DataSource = ds.Tables("Category")
+        Dim series1 As Series = Chart1.Series("Series1")
+        series1.ChartType = SeriesChartType.Pie
+        series1.Name = "CATEGORY"
+
+        With Chart1
+            .Series(series1.Name).XValueMember = "Years"
+            .Series(series1.Name).YValueMembers = "Amount"
+        End With
+
+
+
+        'Try : conn.Open()
+        '    mycommand = conn.CreateCommand
+        '    mycommand.CommandText = "SELECT    SUM(SoldItem.DiscountAmount) as TotalDiscAmount,   SUM(SoldItem.NetAmount) as TotalNetAmount FROM Transactions INNER JOIN (Item INNER JOIN SoldItem ON Item.Barcode = SoldItem.Barcode) ON Transactions.TransID = SoldItem.TransID WHERE ((Transactions.TransDate Like '%" & Format(Date.Now, "Short Date") & "%' )) " '"
+        '    myadapter.SelectCommand = mycommand
+        '    mysqlreader = mycommand.ExecuteReader
+        '    While mysqlreader.Read
+        '        frmAdmin.Chart1.Series("Series1").Points.AddXY(("Profit of the Day").ToString, mysqlreader("TotalNetAmount"))
+        '        frmAdmin.Chart1.Series("Series1").Points.AddXY(("Discounted of the Day").ToString, mysqlreader("TotalDiscAmount"))
+        '    End While : conn.Close()
+        '    conn.Open() : mycommand = conn.CreateCommand
+        '    mycommand.CommandText = "SELECT * FROM Sales where Month ='" & Month(Now.Month) & "' and  Year ='" & (Now.Year).ToString & "'  "
+        '    myadapter.SelectCommand = mycommand
+        '    mysqlreader = mycommand.ExecuteReader
+        '    While mysqlreader.Read
+        '        frmAdmin.Chart1.Series("Series1").Points.AddXY(("Month of " & Month(Now.Month)).ToString, mysqlreader("TotalSales").ToString)
+        '    End While : conn.Close()
+        'Catch ex As Exception
+        'End Try
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
