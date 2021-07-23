75ce5afd08d6b3b341ef99a113ba88151542c92b
enriquesalomon <king92salomon@gmail.com>
2021-07-23 09:31:52 -0700

    Chart

--- Forms/frmChart.Designer.vb
+++ Forms/frmChart.Designer.vb
@@ -41,15 +41,13 @@ Partial Class frmChart
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Chart2 = New System.Windows.Forms.DataVisualization.Charting.Chart()
         Me.Chart3 = New System.Windows.Forms.DataVisualization.Charting.Chart()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.cmbYear = New System.Windows.Forms.ComboBox()
         Me.btnrefresh = New System.Windows.Forms.Button()
+        Me.cmbYear = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel4.SuspendLayout()
         CType(Me.Chart2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Chart3, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Chart1
@@ -60,7 +58,7 @@ Partial Class frmChart
         Me.Chart1.ChartAreas.Add(ChartArea1)
         Legend1.Name = "Legend1"
         Me.Chart1.Legends.Add(Legend1)
-        Me.Chart1.Location = New System.Drawing.Point(192, 32)
+        Me.Chart1.Location = New System.Drawing.Point(9, 336)
         Me.Chart1.Name = "Chart1"
         Series1.ChartArea = "ChartArea1"
         Series1.Legend = "Legend1"
@@ -70,7 +68,7 @@ Partial Class frmChart
         Series2.Name = "Series2"
         Me.Chart1.Series.Add(Series1)
         Me.Chart1.Series.Add(Series2)
-        Me.Chart1.Size = New System.Drawing.Size(649, 293)
+        Me.Chart1.Size = New System.Drawing.Size(932, 332)
         Me.Chart1.TabIndex = 0
         Me.Chart1.Text = "Chart1"
         '
@@ -82,7 +80,7 @@ Partial Class frmChart
         Me.Panel4.Dock = System.Windows.Forms.DockStyle.Top
         Me.Panel4.Location = New System.Drawing.Point(0, 0)
         Me.Panel4.Name = "Panel4"
-        Me.Panel4.Size = New System.Drawing.Size(1099, 26)
+        Me.Panel4.Size = New System.Drawing.Size(954, 26)
         Me.Panel4.TabIndex = 581
         '
         'btnClose
@@ -91,7 +89,7 @@ Partial Class frmChart
         Me.btnClose.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnClose.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.btnClose.ImageIndex = 1
-        Me.btnClose.Location = New System.Drawing.Point(1068, 0)
+        Me.btnClose.Location = New System.Drawing.Point(923, 0)
         Me.btnClose.Name = "btnClose"
         Me.btnClose.Size = New System.Drawing.Size(31, 26)
         Me.btnClose.TabIndex = 218
@@ -120,7 +118,7 @@ Partial Class frmChart
         Me.Chart2.ChartAreas.Add(ChartArea2)
         Legend2.Name = "Legend1"
         Me.Chart2.Legends.Add(Legend2)
-        Me.Chart2.Location = New System.Drawing.Point(12, 375)
+        Me.Chart2.Location = New System.Drawing.Point(9, 60)
         Me.Chart2.Name = "Chart2"
         Series3.ChartArea = "ChartArea1"
         Series3.Legend = "Legend1"
@@ -130,7 +128,7 @@ Partial Class frmChart
         Series4.Name = "Series2"
         Me.Chart2.Series.Add(Series3)
         Me.Chart2.Series.Add(Series4)
-        Me.Chart2.Size = New System.Drawing.Size(536, 293)
+        Me.Chart2.Size = New System.Drawing.Size(463, 270)
         Me.Chart2.TabIndex = 582
         Me.Chart2.Text = "Chart2"
         '
@@ -142,7 +140,7 @@ Partial Class frmChart
         Me.Chart3.ChartAreas.Add(ChartArea3)
         Legend3.Name = "Legend1"
         Me.Chart3.Legends.Add(Legend3)
-        Me.Chart3.Location = New System.Drawing.Point(551, 375)
+        Me.Chart3.Location = New System.Drawing.Point(478, 60)
         Me.Chart3.Name = "Chart3"
         Series5.ChartArea = "ChartArea1"
         Series5.Legend = "Legend1"
@@ -152,49 +150,16 @@ Partial Class frmChart
         Series6.Name = "Series2"
         Me.Chart3.Series.Add(Series5)
         Me.Chart3.Series.Add(Series6)
-        Me.Chart3.Size = New System.Drawing.Size(536, 293)
+        Me.Chart3.Size = New System.Drawing.Size(463, 270)
         Me.Chart3.TabIndex = 583
         Me.Chart3.Text = "Chart3"
-        '
-        'Panel1
-        '
-        Me.Panel1.BackColor = System.Drawing.Color.Khaki
-        Me.Panel1.Controls.Add(Me.btnrefresh)
-        Me.Panel1.Controls.Add(Me.cmbYear)
-        Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Location = New System.Drawing.Point(0, 336)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(1099, 26)
-        Me.Panel1.TabIndex = 584
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.BackColor = System.Drawing.Color.Transparent
-        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(6, 6)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(91, 16)
-        Me.Label1.TabIndex = 137
-        Me.Label1.Text = "FILTER BY YEAR"
-        '
-        'cmbYear
-        '
-        Me.cmbYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.cmbYear.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cmbYear.FormattingEnabled = True
-        Me.cmbYear.Location = New System.Drawing.Point(103, 1)
-        Me.cmbYear.Name = "cmbYear"
-        Me.cmbYear.Size = New System.Drawing.Size(234, 24)
-        Me.cmbYear.TabIndex = 663
         '
         'btnrefresh
         '
         Me.btnrefresh.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnrefresh.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.btnrefresh.Image = CType(resources.GetObject("btnrefresh.Image"), System.Drawing.Image)
-        Me.btnrefresh.Location = New System.Drawing.Point(337, 1)
+        Me.btnrefresh.Location = New System.Drawing.Point(339, 32)
         Me.btnrefresh.Name = "btnrefresh"
         Me.btnrefresh.Size = New System.Drawing.Size(58, 25)
         Me.btnrefresh.TabIndex = 664
@@ -202,12 +167,36 @@ Partial Class frmChart
         Me.btnrefresh.TextAlign = System.Drawing.ContentAlignment.TopCenter
         Me.btnrefresh.UseVisualStyleBackColor = True
         '
+        'cmbYear
+        '
+        Me.cmbYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbYear.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbYear.FormattingEnabled = True
+        Me.cmbYear.Location = New System.Drawing.Point(105, 32)
+        Me.cmbYear.Name = "cmbYear"
+        Me.cmbYear.Size = New System.Drawing.Size(234, 24)
+        Me.cmbYear.TabIndex = 663
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(8, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 16)
+        Me.Label1.TabIndex = 137
+        Me.Label1.Text = "FILTER BY YEAR"
+        '
         'frmChart
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1099, 680)
-        Me.Controls.Add(Me.Panel1)
+        Me.ClientSize = New System.Drawing.Size(954, 680)
+        Me.Controls.Add(Me.btnrefresh)
+        Me.Controls.Add(Me.cmbYear)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Chart3)
         Me.Controls.Add(Me.Chart2)
         Me.Controls.Add(Me.Chart1)
@@ -221,9 +210,8 @@ Partial Class frmChart
         Me.Panel4.PerformLayout()
         CType(Me.Chart2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Chart3, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -233,7 +221,6 @@ Partial Class frmChart
     Friend WithEvents Label5 As Label
     Friend WithEvents Chart2 As DataVisualization.Charting.Chart
     Friend WithEvents Chart3 As DataVisualization.Charting.Chart
-    Friend WithEvents Panel1 As Panel
     Friend WithEvents Label1 As Label
     Friend WithEvents cmbYear As ComboBox
     Friend WithEvents btnrefresh As Button
